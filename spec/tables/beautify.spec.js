4c9bd58f08e356ce5118dd95aaa3c4e00bf6f250
Coding Jerk <codingjerk@gmail.com>
2020-05-12 14:20:33 +0300

    Add simple beautify test

---
+++ spec/tables/beautify.spec.js
@@ -0,0 +1,20 @@
+const beautifyTable = () => {};
+
+test("replaces all numeric values in data", () => {
+  const fakeW = {
+    data: {
+      records: [
+        {"recid": "0", "rowNames": ["Москва"], "column 0": 1200, "column 1": 2300400},
+        {"recid": "1", "rowNames": ["Владивосток"], "column 0": 2300, "column 1": 3400500},
+      ],
+    },
+  };
+
+  beautifyTable(fakeW);
+
+  expect(fakeW.data.records[0]["column 0"]).toBe("1 200");
+  expect(fakeW.data.records[0]["column 1"]).toBe("2 300 400");
+
+  expect(fakeW.data.records[1]["column 0"]).toBe("2 300");
+  expect(fakeW.data.records[1]["column 1"]).toBe("3 400 500");
+});
